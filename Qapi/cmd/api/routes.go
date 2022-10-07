83cbc8945c96d64e47c1d4e489efd4421ee7cba7
michael <2018118090@ub.edu.bz>
2022-10-06 23:54:05 -0600

    added the a new route for the bio handler

--- Qapi/cmd/api/routes.go
+++ Qapi/cmd/api/routes.go
@@ -14,5 +14,6 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/randomstring/:seed", app.makeRandomStringHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/bio", app.bioHandler)
 	return router
 }
